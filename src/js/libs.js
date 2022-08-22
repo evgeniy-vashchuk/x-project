2b8716e4ff00c8c2589ec558a36a5aab9df21608
evgeniy-vashchuk <evgeniy.vashchuk.mail@gmail.com>
2022-08-22 12:33:08 +0300

    some fixes

--- src/js/libs.js
+++ src/js/libs.js
@@ -1,22 +1,3 @@
-// Bootstrap base scripts
-//=require ../../node_modules/bootstrap/js/dist/util/index.js
-//=require ../../node_modules/bootstrap/js/dist/dom/event-handler.js
-//=require ../../node_modules/bootstrap/js/dist/dom/data.js
-//=require ../../node_modules/bootstrap/js/dist/dom/manipulator.js
-//=require ../../node_modules/bootstrap/js/dist/util/config.js
-//=require ../../node_modules/bootstrap/js/dist/base-component.js
-//=require ../../node_modules/bootstrap/js/dist/util/component-functions.js
-//=require ../../node_modules/bootstrap/js/dist/dom/selector-engine.js
-//=require ../../node_modules/bootstrap/js/dist/util/swipe.js
-//=require ../../node_modules/bootstrap/js/dist/util/scrollbar.js
-//=require ../../node_modules/bootstrap/js/dist/util/backdrop.js
-//=require ../../node_modules/bootstrap/js/dist/util/focustrap.js
-//=require ../../node_modules/bootstrap/js/dist/util/sanitizer.js
-//=require ../../node_modules/bootstrap/js/dist/util/template-factory.js
-
-// Bootstrap plugins
-//=require ../../node_modules/bootstrap/js/dist/collapse.js
-
-// Other libs and plugins
+//=require ../../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js
 //=require ../../node_modules/jquery/dist/jquery.min.js
 //=require ../../node_modules/tiny-slider/dist/min/tiny-slider.js
