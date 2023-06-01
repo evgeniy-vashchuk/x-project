453ead4a92330963f4059222858c640f71b898e3
evgeniy-vashchuk <evgeniy.vashchuk.mail@gmail.com>
2023-06-01 15:10:28 +0300

    Some new improvements

--- gulp-tasks/pug.js
+++ gulp-tasks/pug.js
@@ -12,6 +12,7 @@ module.exports = function (gulp, plugins, path, isProduction) {
 				fill_tab: true,
 				tab_size: 2
 			}))
+			.pipe(plugins.formatHtml())
 			//.pipe(plugins.if(isProduction, plugins.htmlmin({collapseWhitespace: true, removeComments: true})))
 			.pipe(plugins.if(isProduction, plugins.replace('css/main.css', 'css/main.min.css')))
 			.pipe(plugins.if(isProduction, plugins.replace('js/libs.js', 'js/libs.min.js')))
