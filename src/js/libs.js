6e0c808118d5af014db44b2554ab1166d8dc22fc
evgeniy-vashchuk <evgeniy.vashchuk.mail@gmail.com>
2024-10-29 20:16:12 +0200

    Some fixes

--- src/js/libs.js
+++ src/js/libs.js
@@ -1,7 +1,7 @@
 /* eslint-disable */
 
-//=require ../../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js
 //=require ../../node_modules/jquery/dist/jquery.min.js
+//=require ../../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js
 //=require ../../node_modules/tiny-slider/dist/min/tiny-slider.js
 
 /* eslint-enable */
