6343c2375b166a8c99cbbe564a6aaa14d30b3e8c
evgeniy-vashchuk <evgeniy.vashchuk.mail@gmail.com>
2024-11-14 18:24:24 +0200

    Fixes for fonts

--- gulp/tasks/fonts.js
+++ gulp/tasks/fonts.js
@@ -3,7 +3,7 @@ import config from '../config.js';
 import { plugins, copyToWordPress } from '../config.js';
 
 const fonts = done => {
-  gulp.src(config.src.fonts)
+  gulp.src(config.src.fonts, { encoding: false })
     .pipe(plugins.destClean(config.dist.fonts))
     .pipe(plugins.newer(config.dist.fonts))
     .pipe(gulp.dest(config.dist.fonts))
