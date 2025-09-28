89e388da030ae65fc33ce846f7667c7f40a0a06d
evgeniy-vashchuk <evgeniy.vashchuk.mail@gmail.com>
2025-09-28 11:15:21 +0300

    Add core-js dependency, clean up SCSS mixins, and adjust dropdown animation duration

--- src/js/utils/constants.js
+++ src/js/utils/constants.js
@@ -3,7 +3,7 @@ export const breakpoints = {
   md: parseInt(getComputedStyle(document.documentElement).getPropertyValue('--bs-breakpoint-md'), 10),
   lg: parseInt(getComputedStyle(document.documentElement).getPropertyValue('--bs-breakpoint-lg'), 10),
   xl: parseInt(getComputedStyle(document.documentElement).getPropertyValue('--bs-breakpoint-xl'), 10),
-  xxl: parseInt(getComputedStyle(document.documentElement).getPropertyValue('--bs-breakpoint-xxl'), 10)
+  xxl: parseInt(getComputedStyle(document.documentElement).getPropertyValue('--bs-breakpoint-xxl'), 10),
 };
 
 export const gridGutterWidth = getComputedStyle(document.documentElement).getPropertyValue('--bs-grid-gutter-width');
